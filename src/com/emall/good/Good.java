059489027d641385946eba6fbcdbab621103847b
lyh-ADT <1043970238@qq.com>
2020-03-09 22:52:54 +0800

    添加content.html的商品列表

---
+++ src/com/emall/good/Good.java
@@ -0,0 +1,58 @@
+package com.emall.good;
+
+import java.util.List;
+
+public class Good {
+	private String goodId;
+	private String userId;
+	private Double price;
+	private Integer number;
+	private String goodName;
+	private String description;
+	private List<String> imgs;
+	
+	public String getGoodId() {
+		return goodId;
+	}
+	public void setGoodId(String goodId) {
+		this.goodId = goodId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Integer getNumber() {
+		return number;
+	}
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	public String getGoodName() {
+		return goodName;
+	}
+	public void setGoodName(String goodName) {
+		this.goodName = goodName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public List<String> getImgs() {
+		return imgs;
+	}
+	public void setImgs(List<String> imgs) {
+		this.imgs = imgs;
+	}
+	
+	
+}
